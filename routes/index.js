1dba2d7a2cdec5b00246ae58fd357947d5ef369e
Noam Shani <Noams156@gmail.com>
2021-11-22 16:57:05 +0100

    updated-verison-login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+const { rawListeners } = require("npmlog");
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  const user= req.session.user 
+  res.render("index", {user});
 });
 
 module.exports = router;
