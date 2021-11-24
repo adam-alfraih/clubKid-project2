70c4c2d157e0e19d2abf18351d7943571e8bd641
kostalexis89 <kostalexis.hosting@gmail.com>
2021-11-24 18:35:52 +0100

    Fixed dates, and editing dates and more+++

---
+++ public/js/mapbox.js
@@ -0,0 +1,78 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWFsZnJheSIsImEiOiJja3dkcGV0cmIwbHRyMnZudGhpNXd0YmlvIn0.wwenpbRDlD1TbMw6e-PvTA';
+const map = new mapboxgl.Map({
+	container: 'map', // container ID
+	style: "mapbox://styles/mapbox/streets-v11", // style URL
+	// doubleClickZoom: false,
+	center: [13.4532813, 52.5329816], // starting position [lng, lat]
+	zoom: 15, // starting zoom
+	// pitch: 100
+});
+
+const nav = new mapboxgl.NavigationControl()
+map.addControl(nav, 'top-left')
+
+// setting a popup
+// const popup = new mapboxgl.Popup({
+// 	closesButton: true
+// })
+
+// popup.setLngLat([13.4532813, 52.5329816])
+// 	.setHTML('<h1>Hello</h1>')
+// 	.addTo(map)
+
+
+
+
+const coords = [
+	[13.405, 52.52],
+	[13.6, 52.6]
+]
+
+coords.forEach(coord => {
+	new mapboxgl.Marker({
+		color: 'green',
+	}).setLngLat(coord)
+		.addTo(map)
+})
+
+const addMarker = e => {
+	new mapboxgl.Marker({
+		color: 'yellow',
+		draggable: true
+	}).setLngLat(e.lngLat)
+		.addTo(map)
+		.on('dragend', event => console.log(event.target._lngLat))
+}
+
+// map.on('click', e => console.log(e.lngLat))
+map.on('click', addMarker)
+
+// new mapboxgl.Marker({
+// 	color: 'red',
+// 	draggable: true
+// }).setLngLat([13.4532813, 52.5329816])
+// 	.addTo(map)
+// 	.on('dragend', event => console.log(event.target._lngLat))
+
+//NEW CODE
+
+// axios.get('/spaeti/')
+//   .then(async res => {
+//     //console.log(res)
+//     const spaetis = res.data
+//     console.log(res.data);
+//     for (let i = 0; i < spaetis.length; i++) {
+//       const coord = [spaetis[i].latitude, spaetis[i].longitude]
+//       await addMarker2(coord)
+//      }
+//   })
+
+//   function addMarker2 (coord) {
+//    console.log(coord);
+//     new mapboxgl.Marker({
+//         color: "red",
+//       })
+//         .setLngLat(coord)
+//         .addTo(map) 
+// }
+//END OF NEW CODE
