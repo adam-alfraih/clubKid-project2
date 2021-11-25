faee51e5383b007deaaa5f333c88a009eeddce13
kostalexis89 <kostalexis.hosting@gmail.com>
2021-11-25 19:43:59 +0100

    Testing a bug with login

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ const GitHubStrategy = require('passport-github').Strategy;
 passport.use(new GitHubStrategy({
 	clientID: process.env.GITHUB_CLIENT_ID,
 	clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	callbackURL: "hhttps://club-kid.herokuapp.com/auth/github/callback"
+	callbackURL: "https://club-kid.herokuapp.com/auth/github/callback"
 },
 	(accessToken, refreshToken, profile, done) => {
 		// User.findOrCreate({ githubId: profile.id }, function (err, user) {
